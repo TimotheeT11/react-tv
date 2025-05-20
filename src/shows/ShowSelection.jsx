6dcd392b1f979091421e5316ad2738a983962b5c
Timothee Thomance <timotheet11@gmail.com>
2025-05-20 18:46:56 -0400

    ShowSelection section of README COMPLETE

--- src/shows/ShowSelection.jsx
+++ src/shows/ShowSelection.jsx
@@ -1,6 +1,14 @@
 import "./shows.css";
 
 /** A navbar that allows users to choose between a list of shows */
-export default function ShowSelection() {
-  return <nav className="shows"></nav>;
+export default function ShowSelection({ tvShows, setSelectedShow }) {
+  return (
+    <nav className="shows">
+      {tvShows.map((tvShow) => (
+        <a className="show" key={tvShow.name} onClick={setSelectedShow(tvShow)}>
+          {tvShow.name}
+        </a>
+      ))}
+    </nav>
+  );
 }
