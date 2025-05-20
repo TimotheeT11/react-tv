ad70e6b9b683154a3301c6aec517788d63957673
Timothee Thomance <timotheet11@gmail.com>
2025-05-20 18:36:57 -0400

    ShowDetails section of README COMPLETE

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
+import { tvShows } from "./shows/data";
+import ShowDetails from "./shows/ShowDetails";
+
 /**
- * React TV is an web streaming platform that allows users to browse
+ * React TV is a web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
 export default function App() {
+  const show = null;
   return (
     <>
       <header>
         <p>React TV</p>
       </header>
-      <main></main>
+      <main>
+        <ShowDetails show={tvShows[0]} tvShows={tvShows} />
+      </main>
     </>
   );
 }
